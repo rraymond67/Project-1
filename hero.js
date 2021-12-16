11767f8b46ee849b59b2277626722ab3a4612c2a
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2021-12-16 16:17:14 -0500

    Website

--- hero.js
+++ hero.js
@@ -2,6 +2,7 @@ const characterDiv = document.querySelector("#character-data");
 const searchSection = document.querySelector("#search");
 const characterHouse = document.querySelector("#info-house");
 const searchInput = document.querySelector("#blank");
+const textBook = document.querySelector('#text-info')
 
 async function listCharacter() {
   try {
@@ -9,7 +10,7 @@ async function listCharacter() {
     const res = await axios.get(url);
     const characterData = res.data; // the brackeet makes sure you acces the info inside the data array it fetches 
     let inputValue = searchInput.value
-    const filterCharacter = characterData.filter(character => character.name === inputValue);
+    const filterCharacter = characterData.filter(character => character.name.toLowerCase().includes(inputValue.toLowerCase()));
     console.log(filterCharacter);
     showCharacterData(filterCharacter);
   } catch (error) {
@@ -30,39 +31,58 @@ function showCharacterData(characters) {
    const characterName = document.createElement('h2');
     characterName.innerText = `${element.name}`;
     characterDiv.appendChild(characterName);
+    characterName.classList.add("name")
 
   // Add poster
     const characterImg = document.createElement("img");
     characterImg.src = element.image;
     characterImg.alt = `Photo of ${element.name}`;
-    characterDiv.appendChild(characterImg); 
+    characterDiv.appendChild(characterImg);
+    characterImg.classList.add("student");
 
-  //add ancestry 
-    const characterAnces = document.createElement('h3');
-    characterAnces.innerText = `${element.ancestry}`
-    characterDiv.appendChild(characterAnces);
+  //add actor 
+    const characterActor = document.createElement('h3');
+    characterActor.innerText = `Actor: ${element.actor}`
+    characterDiv.appendChild(characterActor);
+
+  //add info tex
+    if (element.hogwartsStaff === true) {
+      const characterText = document.createElement('p');
+      characterText.innerText = `${element.name} is a professor at Hogwarts School, from the house of ${element.house}`;
+      textBook.appendChild(characterText);
+      characterText.classList.add("text-book"); 
+    } else {
+    const characterText = document.createElement('p');
+    characterText.innerText = `${element.name} is a ${element.ancestry} student from the house of ${element.house}`;
+    textBook.appendChild(characterText);
+    characterText.classList.add("text-book");
+    }
 
   //add house
     if (element.house === "Ravenclaw") {
       const house = document.createElement("img");
       house.src = "./assets/Raven.png";
       house.alt = "image of the Ravenclaw house logo";
+      house.classList.add("houseImage");
       characterHouse.appendChild(house);
     } else if (element.house === "Hufflepuff") {
       const house = document.createElement("img");
       house.src = "./assets/Hufflepuff.png";
       house.alt = "image of the Hufflepuff house logo";
       characterHouse.appendChild(house);
+      house.classList.add("houseImage");
     } else if (element.house === "Slytherin") {
       const house = document.createElement("img");
       house.src = "./assets/Slytherin.png";
       house.alt = "image of the Slytherin house logo";
       characterHouse.appendChild(house);
+      house.classList.add("houseImage");
     } else if (element.house === "Gryffindor") {
       const house = document.createElement("img");
       house.src = "./assets/Gryffindor.png";
       house.alt = "image of the Gryffindor house logo";
       characterHouse.appendChild(house);
+      house.classList.add("houseImage");
     }
     
 });
@@ -72,9 +92,14 @@ function showCharacterData(characters) {
 searchSection.addEventListener("click", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
+  if (searchInput.value !== "") {
   listCharacter();
   removeCharacter();
   removeHouse();
+  removeText();
+  const book = document.querySelector(".character-list");
+  book.style.display = "block"; 
+  }
 }
 
 function removeCharacter() {
@@ -82,4 +107,7 @@ function removeCharacter() {
 }
 function removeHouse() {
   characterHouse.innerHTML = "";
+}
+function removeText() {
+  textBook.innerHTML = "";
 }
